4203b487c462d721084fba7341c9e9927262cf8b
MDP User <mdp@example.com>
2026-03-10 22:19:04 +0530

    Update Bluetooth characteristic UUID

--- js/services/bluetooth.js
+++ js/services/bluetooth.js
@@ -11,8 +11,8 @@ const BluetoothService = {
     listeners: [],
 
     // BLE UUIDs - These would match your ESP32 firmware
-    SERVICE_UUID: '4fafc201-1fb5-459e-8fcc-c5c9c331914b',
-    CHARACTERISTIC_UUID: 'beb5483e-36e1-4688-b7f5-ea07361b26a8',
+    SERVICE_UUID: '12345678-1234-1234-1234-1234567890ab',
+    CHARACTERISTIC_UUID: 'abcd1234-5678-90ab-cdef-1234567890ab',
 
     /**
      * Initialize Bluetooth service
